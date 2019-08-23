356541455d15913178b588f7d1563d29935dcdf3
malik iscandari <malikiscan@gmail.com>
2019-08-23 08:34:34 -0700

    API all set up

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 const App =() => {
+
+const APP_ID = "86d03d82"
+const APP_KEY = "d4fdeba0a4c092b01a0e145a11163d19"
+const EXAMPLE_REQ = 
+`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+
   return(
     <div className="App">
       <h1>Hello World!</h1>
